96e30fabe81202da6155917352530bdf48f8c5b3
Nguyen Dang Manh <manhnd@amberholdings.vn>
2022-11-10 17:53:34 +0700

    done 10/11/2022

--- src/pages/manage-sof/index.js
+++ src/pages/manage-sof/index.js
@@ -899,7 +899,6 @@ export default function ManageSOF() {
                                 </Select>
                             </FormControl>
                         </div>
-
                     </div>
 
                 </Collapse>
